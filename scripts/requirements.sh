a33c951545d2ce63923bbe331766c85ebddbe58b
cpuccino <thegreatcappuccino@gmail.com>
2021-08-09 23:37:35 +1000

    refactor: Cleaned up requirements script

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+#/ Usage: cluster.sh
+#/
+#/ Description: Install requirements
 
-DEBIAN_FRONTEND=noninteractive
+function run() {
+    DEBIAN_FRONTEND=noninteractive
 
-set -x
+    set -x
 
-dpkg --add-architecture i386 && \
-    apt update && apt upgrade && \
-    apt install --no-install-recommends -y \
-    wget lib32gcc1 lib32stdc++6 libcurl4-gnutls-dev:i386 && \
-    chown -R "$ADMIN_NAME:$ADMIN_NAME" ./ && \
-    apt clean && \
-    rm -rf /tmp/* /var/tmp/* var/lib/apt/lists/*
+    dpkg --add-architecture i386 && \
+        apt update && apt upgrade && \
+        apt install --no-install-recommends -y \
+        wget lib32gcc1 lib32stdc++6 libcurl4-gnutls-dev:i386 && \
+        apt clean && \
+        rm -rf /tmp/* /var/tmp/* var/lib/apt/lists/*
+}
+
+run
